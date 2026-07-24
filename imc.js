bf0c514a54b38a712f9acb2b877cd293f777442b
BIKOUTA Guyverna Mon-Mot Désir <bikoutaguyverna@gmail.com>
2026-07-24 15:46:21 +0100

    feat: ajout des 4 test

--- imc.js
+++ imc.js
@@ -5,8 +5,8 @@ function afficher(html) {
 }
 
 //les variables
-let personWeight = 50; // le poids de la personne
-let Size = 1.8; //la taille de la personne
+let personWeight = 40; // le poids de la personne
+let Size = 1.77; //la taille de la personne
 let IMCResult = 0;
 
 function calcuLIMC(wgt, sz) {
@@ -22,5 +22,38 @@ IMCResult = calcuLIMC(personWeight, Size);
 // on afficher un recap des infos de la personne avec sont imc:
 afficher(
   `<p class="resultat">Votre poid : <strong>${personWeight}kg</strong> et votre taille : <strong>${Size}m</strong></p>
-  <p class="resultat">Votre IMC est de : <strong>${IMCResult}</strong></p>`,
+  <p class="resultat maigreur-text">Votre IMC est de : <strong>${IMCResult}</strong></p>`,
+);
+
+//normal
+personWeight = 36;
+Size = 1.8;
+
+IMCResult = calcuLIMC(personWeight, Size);
+
+afficher(
+  `<p class="resultat">Votre poid : <strong>${personWeight}kg</strong> et votre taille : <strong>${Size}m</strong></p>
+  <p class="resultat normal-text">Votre IMC est de : <strong>${IMCResult}</strong></p>`,
+);
+
+//surpoids
+personWeight = 89;
+Size = 1.79;
+
+IMCResult = calcuLIMC(personWeight, Size);
+
+afficher(
+  `<p class="resultat">Votre poid : <strong>${personWeight}kg</strong> et votre taille : <strong>${Size}m</strong></p>
+  <p class="resultat surpoids-text">Votre IMC est de : <strong>${IMCResult}</strong></p>`,
+);
+
+//obesité
+personWeight = 150;
+Size = 1.9;
+
+IMCResult = calcuLIMC(personWeight, Size);
+
+afficher(
+  `<p class="resultat">Votre poid : <strong>${personWeight}kg</strong> et votre taille : <strong>${Size}m</strong></p>
+  <p class="resultat obese-text">Votre IMC est de : <strong>${IMCResult}</strong></p>`,
 );
